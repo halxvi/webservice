edec4665c528c34f6fb54dd27bb4dfbcb7627619
Kakeru Tsukayama <halxvi@gmail.com>
2019-04-04 01:58:06 +0900

    ページ作成 ログインとメインの画面を作成

---
+++ assets/html/login.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>ログイン画面</title>
+</head>
+<body>
+    <form action="">
+        <p>ユーザー名</p><input type="text" id="user_name">
+        <p>password</p><input type="password" id="password">
+        <input type="button" value="ログイン" onclick="send_data()">
+        <input type="button" value="サインアップ">
+    </form>
+    
+</body>
+
+</html>
+

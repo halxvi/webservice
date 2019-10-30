bd6fda2739c742feaa0cf93006d720a717c1a79c
Kakeru Tsukayama <halxvi@gmail.com>
2019-10-31 05:51:21 +0900

    サインアップのページにherokuの情報を挿入

--- assets/html/signup.php
+++ assets/html/signup.php
@@ -1,34 +1,4 @@
 <?php
-<<<<<<< HEAD
-session_start();
-$db['host'] = "192.168.99.100:13306";
-$db["user"] = "root";
-$db["pass"] = "root";
-$db["dbname"] = "my_system";
-
-$errorMessage = "";
-$signupMessage = "";
-
-if (isset($_POST["signup"])) {
-    $user_name = $_POST["user_name"];
-    $password = Password_hash($_POST["password"], PASSWORD_DEFAULT);
-    $dsn = sprintf('mysql:host=%s; dbname=%s; charset=utf8', $db['host'], $db['dbname']);
-
-    try {
-        $pdo = new PDO($dsn, $db["user"], $db["pass"], array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-        $stmt = $pdo->prepare('INSERT INTO Users(User_Name,User_Password) VALUE(?,?)');
-        $stmt->execute(array($user_name, $password));
-        $signupMessage = "登録が完了しました";
-    } catch (PDOException $e) {
-        $errorMessage = "データベースエラー";
-        echo $e->getmessage();
-    }
-}
-if (isset($_POST["get_back"])) {
-    header("Location: login.php");
-}
-
-=======
 //require_once("config.php");
 
 class SignupController
@@ -53,12 +23,10 @@ class SignupController
         $Password = filter_input(INPUT_POST, 'Password');
         $this->numalphabetchecker($UserName) && $this->strlengthchecker($UserName, 4, 10) ? $UserName = $UserName : $UserName = false;
         $this->numalphabetchecker($Password) && $this->strlengthchecker($Password, 8, 16) ? $Password = password_hash($Password, PASSWORD_DEFAULT) : $Password = false;
-        $dbhostname = getenv('DBHOSTNAME');
-        $dbname = getenv('DBNAME');
-        $dbusername = getenv('DBUSERNAME');
-        $dbpassword = getenv('DBPASSWORD');
-        $dsn = sprintf('mysql:host=%s; dbname=%s; charset=utf8', $dbhostname, $dbname);
-        $pdo = new PDO($dsn, $dbusername, $dbpassword, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        $dbENV = parse_url($_SERVER["CLEARDB_DATABASE_URL"]);
+        $dbENV['dbname'] = ltrim($dbENV['path'], '/');
+        $dsn = sprintf('mysql:host=%s; dbname=%s; charset=utf8', $dbENV['host'], $dbENV['dbname']);
+        $pdo = new PDO($dsn, $dbENV['user'], $dbENV['pass'], array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         if ($UserName && $Password) {
             try {
                 $stmt = $pdo->prepare('INSERT INTO Users(UserName,UserPassword) VALUE(?,?)');
@@ -101,7 +69,6 @@ if (filter_input(INPUT_POST, "Signup")) {
 if (filter_input(INPUT_POST, "getBack")) {
     $signup->getBack();
 }
->>>>>>> 90058cbad6c9ac33a8f59249d96a084ae93d4f5c
 ?>
 
 <html>
@@ -112,25 +79,6 @@ if (filter_input(INPUT_POST, "getBack")) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
-<<<<<<< HEAD
-
-<body>
-    <h1>サインアップ</h1>
-    <?php echo htmlspecialchars($errorMessage, ENT_QUOTES);
-    echo htmlspecialchars($signupMessage, ENT_QUOTES);
-    ?>
-    <form id="signupForm" method="POST">
-        <p>ユーザー名(10字以内)</p><input type="text" id="user_name" name="user_name" maxlength="10" required>
-        <p>password</p><input type="password" id="password" name="password" required>
-        <br>
-        <input type="submit" name="signup" class="btn btn-primary" value="登録">
-    </form>
-    <form method="POST">
-        <input type="submit" name="get_back" class="btn btn-outline-primary" value="戻る">
-    </form>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-=======
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
@@ -170,7 +118,6 @@ if (filter_input(INPUT_POST, "getBack")) {
             </div>
         </div>
     </div>
->>>>>>> 90058cbad6c9ac33a8f59249d96a084ae93d4f5c
 </body>
 
 </html>

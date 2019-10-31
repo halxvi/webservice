d15e517cd22529d097a5b878b7d19ae7d6bbedee
Kakeru Tsukayama <halxvi@gmail.com>
2019-11-01 00:43:49 +0900

    shrink to fitを削除

--- assets/html/main.php
+++ assets/html/main.php
@@ -149,7 +149,7 @@ if (!isset($_SESSION["ID"])) {
 <head>
   <title>もくひょうくん</title>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="css/simple-sidebar.css" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
